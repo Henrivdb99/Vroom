d755e6f577de589f2ab986b538c0b27170a5cc96
Henrivdb99 <henri.vandenberghe@student.hogent.be>
2023-08-03 21:45:17 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,67 +1,67 @@
-// Array of all people
-const people = ['Annelies', 'Margot', 'Henri', 'Jasper', 'Vital', 'Justine', 'Laora', 'Saffi'];
-
-// Fixed seats for drivers
-const driverCar1 = 'Margot';
-const driverCar2 = 'Annelies';
-
-// Function to shuffle an array in place using Fisher-Yates algorithm
-function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
+document.addEventListener("DOMContentLoaded", function () {
+    const randomizeBtn = document.getElementById("randomize-btn");
+    const clearBtn = document.getElementById("clear-btn");
+  
+    // Driver seat positions
+    const driverSeats = {
+      Margot: { top: "46%", left: "20%" },
+      Annelies: { top: "52%", left: "59%" },
+    };
+  
+    // Passenger seat positions
+    const passengerSeats = [
+      { top: "46%", left: "31%" },
+      { top: "52%", left: "70%" },
+      { top: "59%", left: "70%" },
+      { top: "59%", left: "59%" },
+      { top: "53%", left: "20%" },
+      { top: "53%", left: "31%" },
+    ];
+  
+    // Get references to passenger seats
+    const passengerSeatElements = document.querySelectorAll(".seat:not(#Annelies):not(#Margot)");
+  
+    randomizeBtn.addEventListener("click", function () {
+      // Move Annelies and Margot to their driver seats
+      for (const personId in driverSeats) {
+        const seat = document.getElementById(personId);
+        seat.style.top = driverSeats[personId].top;
+        seat.style.left = driverSeats[personId].left;
+      }
+  
+      // Shuffle passenger seat positions
+      shuffleArray(passengerSeats);
+  
+      // Assign shuffled positions to the passengers
+      passengerSeatElements.forEach((seat, index) => {
+        seat.style.top = passengerSeats[index].top;
+        seat.style.left = passengerSeats[index].left;
+      });
+    });
+  
+    clearBtn.addEventListener("click", function () {
+        // Move all persons back to their initial positions
+        const seats = document.querySelectorAll(".seat");
+      
+        seats.forEach((seat) => {
+          // Get the initial top and left positions from the data attributes
+          const initialTop = seat.dataset.initialTop;
+          const initialLeft = seat.dataset.initialLeft;
+      
+          // Set the seat's position to its initial position
+          seat.style.top = initialTop;
+          seat.style.left = initialLeft;
+        });
+      });
+      
+      
+  
+    // Helper function to shuffle an array using Fisher-Yates algorithm
+    function shuffleArray(array) {
+      for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
+      }
     }
-}
-
-// Function to clear the seat names
-function clearSeats() {
-    document.getElementById('car1-driver').textContent = '';
-    document.getElementById('car1-copilot').textContent = '';
-    document.getElementById('car1-rear-left').textContent = '';
-    document.getElementById('car1-rear-right').textContent = '';
-    document.getElementById('car2-driver').textContent = '';
-    document.getElementById('car2-copilot').textContent = '';
-    document.getElementById('car2-rear-left').textContent = '';
-    document.getElementById('car2-rear-right').textContent = '';
-}
-
-// Function to fill seats with random names
-function randomizeSeating() {
-    // Clone the people array and shuffle it (excluding drivers)
-    const passengers = [...people].filter(person => person !== driverCar1 && person !== driverCar2);
-    shuffleArray(passengers);
-
-    // Assign the shuffled passengers to the car seats
-    document.getElementById('car1-copilot').textContent = passengers[0];
-    document.getElementById('car1-rear-left').textContent = passengers[1];
-    document.getElementById('car1-rear-right').textContent = passengers[2];
-    document.getElementById('car2-copilot').textContent = passengers[3];
-    document.getElementById('car2-rear-left').textContent = passengers[4];
-    document.getElementById('car2-rear-right').textContent = passengers[5];
-
-    // Set the driver names
-    document.getElementById('car1-driver').textContent = driverCar1;
-    document.getElementById('car2-driver').textContent = driverCar2;
-}
-
-// Event listener for the randomize button
-document.getElementById('randomize-btn').addEventListener('click', () => {
-    // Clear existing seat names
-    clearSeats();
-
-    // Randomize and fill the seats
-    randomizeSeating();
-});
-
-// Event listener for the clear button
-document.getElementById('clear-btn').addEventListener('click', () => {
-    // Clear the seat names
-    clearSeats();
-});
-
-// Event listener for window onload
-window.onload = () => {
-    // Clear the seat names when the page finishes loading
-    clearSeats();
-
-};
+  });
+  
