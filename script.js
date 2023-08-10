62fc19bac065a3a1c781f3a1878434a71c068d39
Henrivdb99 <henri.vandenberghe@student.hogent.be>
2023-08-10 14:35:29 +0200

    Add Drinking mode

--- script.js
+++ script.js
@@ -1,67 +1,132 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const randomizeBtn = document.getElementById("randomize-btn");
-    const clearBtn = document.getElementById("clear-btn");
-  
-    // Driver seat positions
-    const driverSeats = {
+  const randomizeBtn = document.getElementById("randomize-btn");
+  const clearBtn = document.getElementById("clear-btn");
+  const toggle = document.getElementById("drinking-mode-toggle");
+  const discoLightsContainer = document.getElementById("disco-lights");
+
+
+  // Driver seat positions
+  const driverSeats = {
       Margot: { top: "46%", left: "20%" },
-      Annelies: { top: "52%", left: "59%" },
-    };
-  
-    // Passenger seat positions
-    const passengerSeats = [
+      Laora: { top: "52%", left: "59%" },
+  };
+
+  // Passenger seat positions
+  const passengerSeats = [
       { top: "46%", left: "31%" },
       { top: "52%", left: "70%" },
       { top: "59%", left: "70%" },
       { top: "59%", left: "59%" },
       { top: "53%", left: "20%" },
       { top: "53%", left: "31%" },
-    ];
-  
-    // Get references to passenger seats
-    const passengerSeatElements = document.querySelectorAll(".seat:not(#Annelies):not(#Margot)");
-  
-    randomizeBtn.addEventListener("click", function () {
-      // Move Annelies and Margot to their driver seats
+  ];
+
+  // Get references to passenger seats
+  const passengerSeatElements = document.querySelectorAll(".seat:not(#Laora):not(#Margot)");
+
+  randomizeBtn.addEventListener("click", function () {
+      // Move Laora and Margot to their driver seats
       for (const personId in driverSeats) {
-        const seat = document.getElementById(personId);
-        seat.style.top = driverSeats[personId].top;
-        seat.style.left = driverSeats[personId].left;
+          const seat = document.getElementById(personId);
+          seat.style.top = driverSeats[personId].top;
+          seat.style.left = driverSeats[personId].left;
       }
-  
+
       // Shuffle passenger seat positions
       shuffleArray(passengerSeats);
-  
+
       // Assign shuffled positions to the passengers
       passengerSeatElements.forEach((seat, index) => {
-        seat.style.top = passengerSeats[index].top;
-        seat.style.left = passengerSeats[index].left;
+          seat.style.top = passengerSeats[index].top;
+          seat.style.left = passengerSeats[index].left;
       });
-    });
-  
-    clearBtn.addEventListener("click", function () {
-        // Move all persons back to their initial positions
-        const seats = document.querySelectorAll(".seat");
-      
-        seats.forEach((seat) => {
+
+      // If Drinking Mode is active, swap Annelies and Laora
+      if (toggle.checked) {
+          const anneliesSeat = document.getElementById("Annelies");
+          const laoraSeat = document.getElementById("Laora");
+
+          const anneliesTop = anneliesSeat.style.top;
+          const anneliesLeft = anneliesSeat.style.left;
+
+          anneliesSeat.style.top = laoraSeat.style.top;
+          anneliesSeat.style.left = laoraSeat.style.left;
+
+          laoraSeat.style.top = anneliesTop;
+          laoraSeat.style.left = anneliesLeft;
+      }
+  });
+
+  clearBtn.addEventListener("click", function () {
+      // Move all persons back to their initial positions
+      const seats = document.querySelectorAll(".seat");
+
+      seats.forEach((seat) => {
           // Get the initial top and left positions from the data attributes
           const initialTop = seat.dataset.initialTop;
           const initialLeft = seat.dataset.initialLeft;
-      
+
           // Set the seat's position to its initial position
           seat.style.top = initialTop;
           seat.style.left = initialLeft;
-        });
       });
-      
-      
-  
-    // Helper function to shuffle an array using Fisher-Yates algorithm
-    function shuffleArray(array) {
-      for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-      }
-    }
   });
-  
+
+
+  let discoInterval;
+
+  toggle.addEventListener("change", function () {
+      if (toggle.checked) {
+          startDiscoLights();
+      } else {
+          stopDiscoLights();
+      }
+  });
+
+  function startDiscoLights() {
+      generateDiscoLights(); // Generate initial lights
+      discoInterval = setInterval(updateDiscoLights, 1000); // Update every second
+  }
+
+  function stopDiscoLights() {
+      clearInterval(discoInterval);
+      discoLightsContainer.innerHTML = ""; // Clear existing lights
+  }
+
+  // Helper function to generate disco lights
+  function generateDiscoLights() {
+      discoLightsContainer.innerHTML = ""; // Clear existing lights
+      for (let i = 0; i < 20; i++) {
+          const light = document.createElement("div");
+          light.classList.add("disco-light");
+          light.style.top = `${Math.random() * 100}%`;
+          light.style.left = `${Math.random() * 100}%`;
+          light.style.backgroundColor = getRandomColor();
+          discoLightsContainer.appendChild(light);
+      }
+  }
+
+  // Helper function to update disco lights positions and colors
+  function updateDiscoLights() {
+      generateDiscoLights(); // Generate new set of lights
+  }
+
+  // Helper function to generate a random color
+  function getRandomColor() {
+      const letters = "0123456789ABCDEF";
+      let color = "#";
+      for (let i = 0; i < 6; i++) {
+          color += letters[Math.floor(Math.random() * 16)];
+      }
+      return color;
+  }
+
+
+  // Helper function to shuffle an array using Fisher-Yates algorithm
+  function shuffleArray(array) {
+      for (let i = array.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [array[i], array[j]] = [array[j], array[i]];
+      }
+  }
+});
